b445969f93f3ae5bf4c7f54fa8508365c3cc78c4
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2023-09-05 16:08:16 -0300

    Excluindo Pessoa de main.cpp e incluindo pessoa.h

--- folha-pagamento/main.cpp
+++ folha-pagamento/main.cpp
@@ -1,50 +1,10 @@
 #include <iostream>
 #include <string>
+#include "pessoa.h"
 
 using namespace std;
 
-//Cria a classe Pessoa
- class Pessoa
-    {
-    protected:
-        int cpf;
-        int rg;
-        string nome;
-    public:
-        //Construtor
-        Pessoa(int _cpf, int _rg, string _nome);
 
-        string getNome();
-        void setCpf(int _cpf);
-        void setRg(int _rg);
-        void setNome(string _nome);
-        
-    };
-    //Construtor
-    Pessoa::Pessoa(int _cpf, int _rg, string _nome){
-        cpf = _cpf;
-        rg = _rg;
-        nome = _nome;
-    }
-    
-    void Pessoa::setNome(string _nome)
-    {
-        nome = _nome;
-    }
-
-    string Pessoa::getNome(){
-        return nome;
-    }
-    
-    void Pessoa::setRg(int _rg)
-    {
-        rg = _rg;
-    }
-
-    void Pessoa::setCpf(int _cpf)
-    {
-        cpf = _cpf;
-    }
     
     //Cria a classe Colaborador, que é uma classe filha de Pessoa.
     class Colaborador : public Pessoa{

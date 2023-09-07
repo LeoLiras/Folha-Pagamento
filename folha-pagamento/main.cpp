c36a482af101ae67035a70252433aa345da2b327
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2023-09-07 14:19:02 -0300

    registro() -> incersão de dados.

--- folha-pagamento/main.cpp
+++ folha-pagamento/main.cpp
@@ -7,7 +7,21 @@
 using namespace std;
 
 void registro(){
-    
+    int cpf, rg;
+    string nome;
+    float salario_hora, horas_trabalhadas;
+
+    cout << "Insira o nome do colaborador: "<< endl;
+    cin >> nome;
+    getchar();
+    cout << "Insira o cpf do colaborador: "<< endl;
+    cin >> cpf;
+    cout << "Insira o rg do colaborador: "<< endl;
+    cin >> rg;
+    cout << "Insira o salário por hora do colaborador: "<< endl;
+    cin >> salario_hora;
+    cout << "Insira a quantidade de horas trabalhadas no mês: "<< endl;
+    cin >> nome;
 }
 
 void menu(){

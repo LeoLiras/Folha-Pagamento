cfffc82397e31ce483fed27d13d2630fc13b6ffe
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2023-09-07 13:18:58 -0300

    Implementando menu()

--- folha-pagamento/main.cpp
+++ folha-pagamento/main.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+void menu(){
+    cout << "========== Menu ==========" << endl;
+    cout << "[1] - Iniciar Registro";
+    cout << "[2] - Fechar o programa";
+}
+
 int main(){
     
     return 0;

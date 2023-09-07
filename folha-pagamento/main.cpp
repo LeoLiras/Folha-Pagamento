bcf0f7b4bf996367ec5ce596b7454b9025138697
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2023-09-07 12:56:16 -0300

    Incluindo colaborador.h em main.cpp

--- folha-pagamento/main.cpp
+++ folha-pagamento/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include "colaborador.h"
 
 using namespace std;
 
 int main(){
+    Colaborador c1 = Colaborador(123, 123, "Teste", 50, 44);
 
+    cout << c1.getNome();
     return 0;
 }
